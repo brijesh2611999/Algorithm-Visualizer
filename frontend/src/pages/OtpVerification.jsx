e4d218e0d71cc45fa517060070c417fba570d5b7
Brijesh verma <bv2611999@gmail.com>
2025-04-08 11:06:05 +0530

    update frontend url

--- frontend/src/pages/OtpVerification.jsx
+++ frontend/src/pages/OtpVerification.jsx
@@ -28,7 +28,7 @@ function OtpVerification() {
     try {
       // const url = "http://localhost:4000/api/v1/signup";
       const url = "https://algorithm-visualizer-amx3.onrender.com/api/v1/signup";
-      const response = await fetch("url", {
+      const response = await fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
